678e9dabddfc1c7820248b48f9e7bf01912dce2f
Liam Davis <wjdavisiii@pm.me>
2025-01-11 19:17:23 -0500

    Refactor LoadingScreen component: enhance animation sequences, update layout structure, and improve styling. Replace hardcoded class names with variables for better maintainability. Revise copyright information in the footer. Clean up main CSS by removing unused styles and applying a consistent background color.

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -5,23 +5,34 @@ import { navigate } from 'astro:transitions/client'
 
 const LoadingScreen = () => {
 	useEffect(() => {
-		const tl = gsap.timeline()
+		const timeline = gsap.timeline()
 
-		tl.from('.boot-text', {
-			opacity: 0,
-			y: 20,
-			duration: 0.5,
-			delay: 1,
-			stagger: 0.2
-		})
-			.to({}, { duration: 0.5 })
-			.from('.splash-logo', {
+		const bootScreenSelector = '.boot-screen'
+		const bootTextSelector = '.boot-text'
+		const bootLogoSelector = '.boot-logo'
+
+		timeline
+			.to(bootScreenSelector, {
+				height: '100lvh',
+				ease: 'power2.out',
+				background: '#1c1c1c',
+				delay: 0.5,
+				duration: 0.2
+			})
+			.from(bootTextSelector, {
+				opacity: 0,
+				y: 20,
+				duration: 0.5,
+				delay: 1,
+				stagger: 0.2
+			})
+			.from(bootLogoSelector, {
 				opacity: 0,
 				transform: 'translate3d(-12.5%,0,0)',
 				duration: 0.5,
 				ease: 'power1.out'
 			})
-			.to('.boot-text, .splash-logo', {
+			.to(`${bootTextSelector}, ${bootLogoSelector}`, {
 				opacity: 0,
 				duration: 0.5,
 				delay: 2,
@@ -31,15 +42,15 @@ const LoadingScreen = () => {
 	}, [])
 
 	return (
-		<div className='flex flex-col items-center justify-center h-screen bg-black text-white'>
-			<div className='splash-logo mb-4'>
+		<div className='boot-screen flex flex-col items-center justify-center w-screen h-px bg-black text-white fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+			<div className='boot-logo mb-4'>
 				<Logo />
 			</div>
 
 			<div className='text-center'>
 				<div className='boot-text text-2xl mb-2 font-pixel'>NUTRI•QUEST</div>
-				<div className='boot-text text-sm font-pixel'>© 2024 DAVIS REGENERATIVE</div>
-				<div className='boot-text text-sm font-pixel'>LICENSED BY WJD3</div>
+				<div className='boot-text text-sm font-pixel'>© 2024 WJD3</div>
+				<div className='boot-text text-sm font-pixel'>LICENSED BY DAVIS REGENERATIVE</div>
 			</div>
 		</div>
 	)
